256788d31bd529f5e0772a7b3e538789927fdbe3
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-07-16 10:34:15 -0300

    Rotas e funções do carrinho

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
+import cartRouter from "./cartRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
+router.use(cartRouter);
 
 export default router;
